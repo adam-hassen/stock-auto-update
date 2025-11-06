a6ca679fb67d506bed4c8bcfb86f11b206a0026c
adam <100930559+adam-hassen@users.noreply.github.com>
2025-11-06 10:27:58 +0100

    Fix GitHub Actions user configuration in commit function

--- stock_pipeline.py
+++ stock_pipeline.py
@@ -60,7 +60,7 @@ def collect_tiingo():
 def commit_and_push():
     print("Commit + Push sur GitHub...")
     subprocess.run(["git", "config", "--global", "adam.hassen@esprit.tn", "actions@github.com"])
-    subprocess.run(["git", "config", "--global", "adam-hassen", "GitHub Actions"])
+    subprocess.run(["git", "config", "--global", "adam", "GitHub Actions"])
     subprocess.run(["git", "add", DATA_FOLDER])
     subprocess.run(["git", "commit", "-m", f"Update prix {datetime.now().strftime('%Y-%m-%d')}"])
     subprocess.run(["git", "push"])
